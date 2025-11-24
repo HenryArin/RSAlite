33e76e6f19701436a751f3d8292a955917d14f1e
HenryArin <128102839+HenryArin@users.noreply.github.com>
2025-11-23 16:12:49 -0800

    Update CI workflow to use Makefile and run tests

--- factor.h
+++ factor.h
@@ -1,6 +1,8 @@
 #ifndef FACTOR_H
 #define FACTOR_H
 
+
+
 //void get_factors(int x);
 int factor_with_sqrt(int n, int *factors, int max_factors);
 int factor_with_trail(int n, int *factors, int max_factors);
